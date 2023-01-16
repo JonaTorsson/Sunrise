5cde00ebea91dfda082dde9fe948d40361498bd9
JonaTorsson <jona.torsson@gmail.com>
2023-01-16 15:49:17 +0100

    get water temp service file

---
+++ src/services/SMHI_API.js
@@ -0,0 +1,39 @@
+import  API_SERVICE  from "./API_SERVICE"
+import { stationIds } from "../helpers/stations";
+
+const api = API_SERVICE("https://opendata-download-ocobs.smhi.se/api/");
+
+export const getWaterTemp = async (stationId = null) => {
+	if (!stationId) return;
+
+	const { data } = await api.get(
+	`version/latest/parameter/5/station/${stationId}/period/latest-day/data.json`
+	);
+
+	const tempValue = data.value[0].value;
+	const tempUnit = data.parameter.unit;
+	return { tempValue, tempUnit };
+};
+
+export const getAllStations = async () => {
+	const result = await api.get("version/1.0/parameter/5.json");
+
+	const { station } = result.data;
+
+	const formattedStations = station
+	.filter((item) => {
+		if (stationIds.find((stationId) => stationId === item.id)) {
+		return item;
+		}
+	})
+	.map((item) => {
+		return {
+		name: item.name,
+		lat: item.latitude,
+		lon: item.longitude,
+		stationId: item.id,
+		};
+	});
+
+	return formattedStations;
+};
