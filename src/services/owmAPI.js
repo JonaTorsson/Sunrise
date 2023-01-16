370cbe73e53ee3c9dd20cefa305121e420408843
JonaTorsson <jona.torsson@gmail.com>
2023-01-16 15:48:43 +0100

    Updated openweather service file

--- src/services/owmAPI.js
+++ src/services/owmAPI.js
@@ -1,10 +1,10 @@
-import { axios } from "./Axios"
+import  API_SERVICE  from "./API_SERVICE"
 
 const weatherApiKey = import.meta.env.VITE_OPEN_WEATHER_MAP_KEY
 
-const API = axios("https://api.openweathermap.org/data/2.5")
+const API = API_SERVICE("https://api.openweathermap.org/data/2.5")
 
-const GetLocationWeather = async (coordinates = null) => {
+const getLocationWeather = async (coordinates = null) => {
 	if (!coordinates) return
 
 	const { lat, lng } = coordinates
@@ -16,29 +16,29 @@ const GetLocationWeather = async (coordinates = null) => {
 }
 
 const getForecast = async (coordinates = null) => {
-	if (!coordinates) return;
+	if (!coordinates) return
 
 	const { lat, lng } = coordinates;
 
-	const { data } = await api.get(
-		`/onecall?lat=${lat}&lon=${lng}&exclude=current,minutely,hourly,alerts&appid=${weatherApiKey}`
-	);
-	return data;
+	const { data } = await API.get(
+		`/onecall?units=metric&lat=${lat}&lon=${lng}&exclude=current,minutely,hourly,alerts&appid=${weatherApiKey}`
+	)
+	return data
 
-	};
+	}
 
 const getCoordinates = async (location) => {
-	if (!location) return;
+	if (!location) return
 
-	const { data } = await api.get(
+	const { data } = await API.get(
 		`/geo/1.0/direct?q=${location},SE&limit=1&appid=${weatherApiKey}`
-	);
+	)
 
-	return data[0];
-};
+	return data[0]
+}
 
 export {
-	GetLocationWeather,
+	getLocationWeather,
 	getForecast,
 	getCoordinates
 }
