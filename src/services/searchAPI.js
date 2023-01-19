5d858f25d2f412251bb99e27aa88809dc8645164
JonaTorsson <jona.torsson@gmail.com>
2023-01-19 14:45:36 +0100

    tested an api for search

---
+++ src/services/searchAPI.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bf6f727b5amsh9252f19b04d3d65p1b4478jsn08fae4d808b2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+} 
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions'
