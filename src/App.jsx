d89bdafa48d8dd5c354ca6778967d981e95dd5bc
JonaTorsson <jona.torsson@gmail.com>
2023-01-19 14:44:20 +0100

    routes and requireAuth

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import NotFound from './pages/NotFound'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import './assets/scss/App.scss'
-import SearchPage from './pages/SearchPage'
+import MapPage from './pages/MapPage'
+import LogoutPage from './pages/LogoutPage'
+import UpdateProfilePage from './pages/UpdateProfilePage'
+import WeatherStatusPage from './pages/WeatherStatusPage'
+import RequireAuth from './components/RequireAuth'
+import ProfilePage from './pages/ProfilePage'
+
 
 function App() {
 	return (
@@ -16,7 +22,38 @@ function App() {
 				<Route path="/" element={<HomePage />} />
 				<Route path="signup" element={<SignupPage />} />
 				<Route path="login" element={<LoginPage />} />
-				<Route path='search' element={<SearchPage />} />
+
+				<Route path="profile" element={
+					<RequireAuth>
+						<ProfilePage />
+					</RequireAuth>
+				} />
+
+
+				<Route path="update-profile" element={
+					<RequireAuth>
+						<UpdateProfilePage/>
+					</RequireAuth>
+				} />
+				
+				<Route path="logout" element={<LogoutPage />} />
+
+				<Route path='map' element={
+					<RequireAuth>
+						<MapPage />
+					</RequireAuth>
+				} />
+				<Route path='map/:lon/:lat' element={
+					<RequireAuth>
+						<WeatherStatusPage />
+					</RequireAuth>
+				} />
+				<Route path='map/:lon/:lat/:locationId' element={
+					<RequireAuth>
+						<WeatherStatusPage />
+					</RequireAuth>
+				} />
+
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
