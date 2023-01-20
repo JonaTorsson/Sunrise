5f9706cbf57c52657268d820c9d309c8d04a38ef
JonaTorsson <jona.torsson@gmail.com>
2023-01-20 14:10:42 +0100

    placeholder image and button

---
+++ src/components/Dropzone/PlaceholderImage.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import UploadImgModal from '../Modal/UploadImgModal'
+import useModal from '../../hooks/useModal'
+import useUploadImg from '../../hooks/useUploadImg'
+import { useQueryClient } from 'react-query'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from 'react-bootstrap'
+
+const PlaceholderImage = ({ locationId }) => {
+	const queryClient = useQueryClient()
+	const { show, openModal, closeModal } = useModal()
+	const fileUploader = useUploadImg()
+
+	useEffect(() => {
+		if (fileUploader.progress) {
+			setTimeout(() => {
+				closeModal()
+				queryClient.invalidateQueries('images')
+			}, 3000)
+		}
+	}, [fileUploader.progress])
+
+	return (
+		<>
+			{!locationId && (
+				<Button 
+				onClick={openModal}
+			>
+				Ladda upp bild
+			</Button>
+			)}
+			
+			<UploadImgModal 
+				fileUploader={fileUploader}
+				show={show}
+				text='image'
+				locationId={locationId}
+				handleClose={closeModal}
+			/>
+		</>
+	)
+}
+
+export default PlaceholderImage
