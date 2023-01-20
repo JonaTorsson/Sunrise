c1b1f8b44ad1d7b49c557276381004eeb0e9f303
JonaTorsson <jona.torsson@gmail.com>
2023-01-20 14:15:50 +0100

    favorites context

---
+++ src/contexts/FavoritesContext.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useState } from 'react'
+
+const FavoritesContext = createContext()
+
+export const useFavoritesContext = () => {
+	return useContext(FavoritesContext)
+}
+
+const FavoritesContextProvider = ({ children }) => {
+	const [ counter, setCounter ] = useState(0)
+
+	const updateCounter = () => {
+		setCounter((prev) => prev + 1)
+	}
+
+	const values = {
+		counter,
+		updateCounter,
+	}
+
+	return (
+		<FavoritesContext.Provider value={ values }>
+			{children}
+		</FavoritesContext.Provider>
+	)
+}
+export default FavoritesContextProvider
