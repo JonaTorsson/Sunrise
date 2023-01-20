f9aac70a5e8965e7287c92c0971c34c6cb350173
JonaTorsson <jona.torsson@gmail.com>
2023-01-20 13:51:32 +0100

    hook to get watertemp

---
+++ src/hooks/useWaterTemp.js
@@ -0,0 +1,12 @@
+import { useQuery } from "react-query"
+import { getWaterTemp } from "../services/SMHI_API"
+
+const useWaterTemp = (stationId = null) => {
+	const temp = useQuery(["watertemp", stationId], () =>
+		getWaterTemp(stationId)
+	)
+
+	return temp
+}
+
+export default useWaterTemp
