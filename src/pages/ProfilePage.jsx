bbe182620446ec3aeb05e05e29267ac40728b501
JonaTorsson <jona.torsson@gmail.com>
2023-04-10 22:33:02 +0200

    small update

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
 import FavoritesWrapper from '../components/Favorite/FavoritesWrapper'
-// import { useFavoritesContext } from '../contexts/FavoritesContext'
 
 const ProfilePage = () => {
-	// const counter  = useFavoritesContext()
 	
 	return (
 		<Col>
